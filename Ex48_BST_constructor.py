3615d81a0b20fcf398bbbc50835e6fbeb13948b1
Harrybeaty <1beatyh@gmail.com>
2024-03-23 09:29:17 +0000

    Exercises 45-48 stacks, queues, and BST

---
+++ Ex48_BST_constructor.py
@@ -0,0 +1,32 @@
+# class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    #################################
+        
+
+# class BinarySearchTree:
+    ## WRITE BST CONSTRUCTOR HERE ##
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    ################################
+
+
+
+
+my_tree = BinarySearchTree()
+
+print("Root:", my_tree.root)
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Root: None
+
+"""
